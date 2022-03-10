51f4fd451514bf622253f7bb3bbe49d7322f6736
taketo0722 <75084869+taketo0722@users.noreply.github.com>
2022-03-10 14:08:57 +0900

    wip move enemy

--- rubycamp_example/directors/game_director.rb
+++ rubycamp_example/directors/game_director.rb
@@ -80,11 +80,11 @@ module Directors
 			rejected_enemies.each{|enemy| self.scene.remove(enemy.mesh) }
 
 			# 一定のフレーム数経過毎に敵キャラを出現させる
-			if @frame_counter % 180 == 0
+			if @frame_counter % 180 == 0 && @enemies.length < 7
 				x = rand(self.camera.position.x..@saisen.position.x)
 				y = rand(self.camera.position.y..@saisen.position.y)
 				z = rand(self.camera.position.z..@saisen.position.z)
-				enemy = Enemy.new(x: x, y: y, z: z, saisen_position: @saisen.position)
+				enemy = Enemy.new(x: x, y: y, z: z, saisen_position: @saisen.position, frame_counter: @frame_counter)
 				@enemies << enemy
 				self.scene.add(enemy.mesh)
 			end
--- rubycamp_example/lib/enemy.rb
+++ rubycamp_example/lib/enemy.rb
@@ -3,7 +3,7 @@ class Enemy
 	attr_accessor :mesh, :expired
 
 	# 初期化
-	def initialize(x: nil, y: nil, z: nil, saisen_position: nil)
+	def initialize(x: nil, y: nil, z: nil, saisen_position: nil, frame_counter: nil)
 		# 初期位置指定が無ければランダムに配置する
 #		x ||= rand(10) / 10.0 - 0.5
 #		y ||= rand(10) / 10.0 + 1
@@ -11,6 +11,7 @@ class Enemy
 #			@earth.position.y = -0.9
 #			@earth.position.z = -0.8
 
+		@saisen_position = saisen_position
 		r = rand(2.0..2.1)
 		y = -0.5
 
@@ -18,6 +19,7 @@ class Enemy
 		self.mesh = MeshFactory.create_enemy(radius_top: 0.0, color: 0x000000)
 		self.mesh.position = pos
 		self.expired = false
+		@radian = 0
 	end
 
 	# メッシュの現在位置を返す
@@ -27,9 +29,23 @@ class Enemy
 
 	# 1フレーム分の進行処理
 	def play
-		radian = rand
+
+		@radian += 0.01
 			# 角度に応じてカメラの位置を設定
-		self.mesh.position.x = 3*Math.sin(radian) + saisen.position.x
-		self.mesh.position.z = 3*Math.cos(radian) + saisen.position.z
+		self.mesh.position.x = 1.5*Math.sin(@radian) + @saisen_position.x
+		self.mesh.position.z = 1.5*Math.cos(@radian) + @saisen_position.z
+
+		self.mesh.position.x = coordinate_x
+		self.mesh.position.z = coordinate_z
+
 	end
+
+	def coordinate_x
+		return @saisen_position.x
+	end
+
+	def coordinate_z
+		return @saisen_position.z
+	end
+
 end
